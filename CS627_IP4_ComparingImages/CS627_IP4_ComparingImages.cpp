fe92a16c2f4e436cf9f7f6355cf41c644b428d8e
Walter Ingham <wally18025@outlook.com>
2019-12-11 05:43:27 -0600

    works

--- CS627_IP4_ComparingImages/CS627_IP4_ComparingImages.cpp
+++ CS627_IP4_ComparingImages/CS627_IP4_ComparingImages.cpp
@@ -1,21 +1,56 @@
-// CS627_IP4_ComparingImages.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// CS627_IP4_ComparingImages.cpp
 
 #include "pch.h"
 #include <iostream>
+using namespace std;
+
+int const jCol = 4;
+int const jRow = 4;
+int const kCol = 4;
+int const kRow = 5;
+int const jTotal = jCol * jRow; // (image 1 row) x (image 1 column) = the total elements in image 1 array
+int const kTotal = kCol * kRow; // (image 2 row) x (image 2 column) = the total elements in image 2 array
+
+int comparison(int image1[], int image2[], int i, int j, int k)
+{ 
+	i = 0;// needs to increment 16
+	j = 0;//needs to increment 16
+	k = 0;// needs to increment 20, skipping 1 space after every 4th element
+
+	if (jTotal<kTotal)
+	{
+		while (j<jTotal)
+		{
+			
+			for(j = 0; j<jCol; j++)
+			{
+				cout << image1[j]<<" ";
+				cout << image2[k];
+				k++;
+			}
+			//k++;
+			return 0;
+		}
+		
+	}
+	else if (j > k)
+	{
+		cout << "j > k";
+	}
+	else
+	{
+		cout << "j == k";
+	}
+	
+	return 0;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int i = 0, j = 0, k = 0;
+	//        [c][r]
+	int image1[jCol][jRow] = { {0,1,1,0}, {0,1,0,1}, {0,1,1,0}, {1,0,1,1} };
+	int image2[kCol][kRow] = { {0,1,0,0,1}, {0,1,1,1,0}, {1,1,1,0,1}, {1,0,1,1,0} };
+	comparison(image1[j], image2[k], i, j, k);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
